9f9e794bb36fc8cd4882aa05f421b6f4f6839c5a
Jennifer Tristan <31359254+jt6161@users.noreply.github.com>
2017-11-27 11:35:43 -0700

    first changes

---
+++ CNN - Breaking News, Latest News and Videos_files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "PROSPER",
+      'continent': "NA",
+      'country': "US",
+      'region': "TX"
+    },
+    'ip':"64.125.67.66"
+  }]);
+})
+//
+()
+
+;
